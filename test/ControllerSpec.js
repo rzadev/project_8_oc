0c689981fcb1d777433e7aaa760321e97969cfbb
creative301 <rza9001@gmail.com>
2019-09-26 19:58:21 +0700

    Add a test

--- test/ControllerSpec.js
+++ test/ControllerSpec.js
@@ -160,9 +160,8 @@ describe('controller', function () {
 
 	it('should highlight "Active" filter when switching to active view', function () {
 		// TODO: write test
-		// look filter and see active
-
-
+		subject.setView("/active");
+		expect(view.render).toHaveBeenCalledWith("setFilter", "active");
 	});
 
 	describe('toggle all', function () {
